84053b2e8e274509e7b844823e1353b05bc98987
Gage Harmon <gaharmon1@gmail.com>
2023-01-31 16:00:32 -0700

    Array Find Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+]
+
+function superbowlWin(Array) {
+    for (let item of Array) {
+        if (item.result === "W") { return item.year; }
+    }
+}
